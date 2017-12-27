24e910949fd5727189a25c1bf6fbbc1c34a06167
Alana Ruth <32655088+alanaruth007@users.noreply.github.com>
2017-12-27 00:12:28 -0500

    Create prototypes.h

---
+++ prototypes.h
@@ -0,0 +1,11 @@
+/*
+Prototypes of browse and index functions
+*/
+
+void browseDirectories(char *directoryName);
+void browseFile(char *fileName);
+void indexGenerator(char *fileName);
+void *checkMalloc(size_t numBytes);
+void freeMem(void *p);
+                                                                                              \
+
